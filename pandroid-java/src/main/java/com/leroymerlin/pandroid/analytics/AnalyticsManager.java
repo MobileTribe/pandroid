22e679aaa354170bc8fba91da946f47eef6c7f4e
paillardf <paillard.f@gmail.com>
2016-07-13 10:46:12 +0200

    refactor (AnalyticsManager): refactor analytics manager + add Firebase Implementation

--- pandroid-java/src/main/java/com/leroymerlin/pandroid/analytics/AnalyticsManager.java
+++ pandroid-java/src/main/java/com/leroymerlin/pandroid/analytics/AnalyticsManager.java
@@ -1,144 +1,59 @@
 package com.leroymerlin.pandroid.analytics;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 
 /**
  * Created by florian on 06/11/14.
  */
 public interface AnalyticsManager {
 
-    public void sendEvent(String categoryId, String action, String label, long value);
-
-    public EventBuilder buildHitEvent(String categoryId, String action, String label, long value);
-
-    public void sendScreen(String screen);
-
-    public EventBuilder buildScreenEvent(String screen);
-
-    public void sendTiming(String category, long duration, String variable, String label);
-
-    public EventBuilder buildTimingEvent(String category, long duration, String variable, String label);
-
-    public void addTracker(Tracker tracker);
-
-    public void send(Event event);
-
-    public TrackerBuilder newTrackerBuilder();
-
-    public EventBuilder newEventBuilder();
-
-    public interface Tracker {
-        void create();
-    }
-
-    abstract class TrackerBuilder {
-
-        protected String id;
-        protected boolean autoTrack = false;
-        protected Integer resources;
-        protected String name;
-        protected HashMap<String,String> parameters;
-
-        public TrackerBuilder setId(String id) {
-            this.id = id;
-            return this;
-        }
-
-        public TrackerBuilder setName(String name) {
-            this.name = name;
-            return this;
-        }
-
-        public TrackerBuilder setResources(int resources) {
-            this.resources = resources;
-            return this;
-        }
-
-        public TrackerBuilder enableAutoTracking(boolean autoTrack) {
-            this.autoTrack = autoTrack;
-            return this;
-        }
-
-        public TrackerBuilder setParameters(HashMap<String, String> parameters) {
-            this.parameters = parameters;
-            return this;
-        }
-
-        public abstract Tracker build();
-    }
-
-    public interface Event {
-        void send();
-    }
-
-    abstract class EventBuilder {
-
-        protected String category;
-        protected String label;
-        protected String screen;
-        protected Long duration;
-        protected Long value;
-        protected String trackerName;
-        protected String variable;
-        protected boolean newSession;
-        protected String action;
-        protected HashMap<Object,Object> params = new HashMap<>();
 
 
-        public EventBuilder setCategory(String category) {
-            this.category = category;
-            return this;
-        }
+    String EVENT_TYPE = "event_type";
 
-        public EventBuilder setLabel(String label) {
-            this.label = label;
-            return this;
-        }
+    String EVENT_CATEGORY = "event_category";
+    String EVENT_VARIABLE = "event_variable";
+    String EVENT_ACTION = "event_action";
+    String EVENT_LABEL = "event_label";
+    String EVENT_VALUE = "event_value";
+    String EVENT_DURATION = "event_duration";
 
-        public EventBuilder setAction(String action) {
-            this.action = action;
-            return this;
-        }
+    String USER_ID = "user_id";
 
-        public EventBuilder setScreen(String screen) {
-            this.screen = screen;
-            return this;
-        }
+    String NAVIGATION_PATH = "navigation_path";
+    String HISTORY = "history";
+    String NEW_SESSION = "new_session";
+    String SCREEN_NAME = "screen_name";
+    String SUBJECT_OBJECT = "subject_object";
 
-        public EventBuilder setValue(long value) {
-            this.value = value;
-            return this;
-        }
-
-        public EventBuilder setVariable(String variable) {
-            this.variable = variable;
-            return this;
-        }
+    AnalyticsTracker track();
 
 
-        public EventBuilder setDuration(long duration) {
-            this.duration = duration;
-            return this;
-        }
+    abstract class AnalyticsProcessor {
 
-        public EventBuilder setTracker(String trackerName) {
-            this.trackerName = trackerName;
-            return this;
-        }
-
-        public EventBuilder setNewSession(boolean newSession) {
-            this.newSession = newSession;
-            return this;
-        }
-
-        public EventBuilder addParam(Object key, Object value) {
-            this.params.put(key, value);
-            return this;
-        }
-
-
-        public abstract Event build();
-
+        public abstract void processParam(HashMap<String, Object> param);
 
     }
+
+    class AnalyticsTracker {
+        protected final AnalyticsProcessor processor;
+
+        protected HashMap<String, Object> params = new LinkedHashMap<>();
+
+        public AnalyticsTracker(AnalyticsProcessor processor) {
+            this.processor = processor;
+
+        }
+
+        public AnalyticsTracker addParam(String key, Object value) {
+            params.put(key, value);
+            return this;
+        }
+
+        public void send() {
+            processor.processParam(params);
+        }
+    }
+
 }
