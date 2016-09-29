3f285cc5af1787090d725c6f6bd8b3a669a51a87
paillardf <paillard.f@gmail.com>
2016-09-29 16:58:33 +0200

    refactor (MVVN) : refactor mvvnViewModel to LifecycleDelegate

--- pandroid-demo/src/main/java/com/leroymerlin/pandroid/demo/main/mvvm/MvvmFragment.java
+++ pandroid-demo/src/main/java/com/leroymerlin/pandroid/demo/main/mvvm/MvvmFragment.java
@@ -2,46 +2,46 @@ package com.leroymerlin.pandroid.demo.main.mvvm;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
-import com.leroymerlin.pandroid.demo.BR;
+import com.leroymerlin.pandroid.app.PandroidFragment;
+import com.leroymerlin.pandroid.app.delegate.PandroidDelegate;
 import com.leroymerlin.pandroid.demo.R;
 import com.leroymerlin.pandroid.event.FragmentOpener;
-import com.leroymerlin.pandroid.mvvm.Layout;
-import com.leroymerlin.pandroid.mvvm.PandroidVMFragment;
-import com.leroymerlin.pandroid.mvvm.ViewModelId;
 
 /**
  * Created by adrien on 14/09/16.
  */
-@Layout(R.layout.fragment_mvvm)
-@ViewModelId(BR.productViewModel)
-public class MvvmFragment extends PandroidVMFragment<FragmentOpener, ProductViewModel, ProductViewModelContract> implements ProductViewModelContract {
+public class MvvmFragment extends PandroidFragment<FragmentOpener> implements MvvmViewModel.ExampleViewContract {
 
     @Override
-    public ProductViewModelContract initViewModelContract() {
-        return this;
+    protected PandroidDelegate createDelegate() {
+        PandroidDelegate delegate = super.createDelegate();
+        delegate.addLifecycleDelegate(new MvvmViewModel());
+        return delegate;
     }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_mvvm, container, false);
+    }
+
 
     @Override
-    public ProductViewModel initViewModel(ProductViewModelContract viewModelContract) {
-        return new ProductViewModel(viewModelContract);
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        viewModel.loadProduct();
-    }
-
-    public void onSubmit(View view) {
+    public void onSubmit(final ProductDataBinding productDataBinding) {
         AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getActivity());
-        alertDialogBuilder.setTitle("" + viewModel.name.get()).setMessage(viewModel.name.get()).setPositiveButton("Oui", new DialogInterface.OnClickListener() {
+        String title = productDataBinding.name.get();
+        alertDialogBuilder.setTitle(title).setMessage(title).setPositiveButton("Oui", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int which) {
             }
         }).setNegativeButton("Non", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int which) {
-                viewModel.name.set("wrong");
+                productDataBinding.name.set("wrong");
 
             }
         }).show();
