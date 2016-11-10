05a50c746c3d1bb81e40c256f63e307a18ca9409
Florian Paillard <florian.paillard@leroymerlin.fr>
2016-11-10 16:17:54 +0100

    docs (LifecycleDelegate) : update doc

--- pandroid-annotations/src/main/java/com/pandroid/annotations/BindLifeCycleDelegate.java
+++ pandroid-annotations/src/main/java/com/pandroid/annotations/BindLifeCycleDelegate.java
@@ -4,10 +4,8 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
 
 @Target(ElementType.FIELD)
-//tag::PandroidBindLifeCycleDelegateAnnotation[]
 public @interface BindLifeCycleDelegate {
 
     String BINDER_PREFIX = "_LifecycleAutoBinder";
 
 }
-//end::PandroidBindLifeCycleDelegateAnnotation[]
