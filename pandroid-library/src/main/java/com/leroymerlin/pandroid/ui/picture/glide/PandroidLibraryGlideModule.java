0846ddccd3fcd931c24fa3ebcb32c54cc30f6e35
paillardf <paillard.f@gmail.com>
2018-03-09 12:21:44 +0100

    feat (Glide) : auto configure Glide module to use OKHttpClient

---
+++ pandroid-library/src/main/java/com/leroymerlin/pandroid/ui/picture/glide/PandroidLibraryGlideModule.java
@@ -0,0 +1,40 @@
+package com.leroymerlin.pandroid.ui.picture.glide;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.Registry;
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.load.model.GlideUrl;
+import com.bumptech.glide.module.LibraryGlideModule;
+import com.leroymerlin.pandroid.PandroidApplication;
+import com.leroymerlin.pandroid.ui.picture.glide.okhttp.OkHttpUrlLoader;
+
+import java.io.InputStream;
+
+import okhttp3.OkHttpClient;
+
+/**
+ * Created by florian on 08/03/2018.
+ */
+@GlideModule
+public class PandroidLibraryGlideModule extends LibraryGlideModule {
+
+
+    @Override
+    public void registerComponents(@NonNull Context context, @NonNull Glide glide,
+                                   @NonNull Registry registry) {
+        setupOkHttpClient(context, registry);
+    }
+
+    static boolean setupOkHttpClient = false;
+
+    protected static void setupOkHttpClient(@NonNull Context context, @NonNull Registry registry) {
+        if (!setupOkHttpClient) {
+            setupOkHttpClient = true;
+            OkHttpClient.Builder builder = PandroidApplication.getInjector(context).getBaseComponent().okHttpClientBuilder();
+            registry.replace(GlideUrl.class, InputStream.class, new OkHttpUrlLoader.Factory(builder.build()));
+        }
+    }
+}
